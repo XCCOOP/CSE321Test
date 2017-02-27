f78cb3461909283997e9d0df0812e1f5e4c25e62
coopern3 <coopern3@miamioh.edu>
2017-02-26 20:33:29 -0500

    Adding files

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	String printStr;
+	
+	public HelloWorld(){
+		printStr = "Hello World!";
+	}
+	
+	public String printString(){
+		System.out.println(printStr);
+		return printStr;
+	}
+
+}
